6eccccb40fb715dbac92961e06fe27d3c60688fe
Ankan Saha <ankansahadaluabari@gmail.com>
2025-09-14 02:27:05 +0530

    Refactor project structure: move DNS server entry point to Config directory and update references

--- Web/src/Config/DNS.ts
+++ Web/src/Config/DNS.ts
@@ -1,5 +1,5 @@
 // Handle incoming DNS queries and respond accordingly
-import DNS from "./services/DNS.Service";
+import DNS from "../services/DNS.Service";
 
 // Start the DNS server
 export default new DNS().start().listen().listenError();
