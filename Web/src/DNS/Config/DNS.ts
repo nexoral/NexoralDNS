b0ee12ab65d77cbbeb0bdf97f5fb3aa3d6d21bf5
Ankan Saha <ankansahadaluabari@gmail.com>
2025-09-15 23:55:33 +0530

    feat(database): integrate Axiodb for database management
    - Added Axiodb as a dependency in package.json.
    - Created Axiodb configuration file to initialize a new database instance.
    - Updated DNS server configuration to create a new database upon server start.

--- Web/src/DNS/Config/DNS.ts
+++ Web/src/DNS/Config/DNS.ts
@@ -1,7 +1,9 @@
 // Handle incoming DNS queries and respond accordingly
+import NewNexoralDB from "../../Database/Axiodb.config";
 import DNS from "../services/DNS.Service";
 
 export default async function startDNSServer() {
+  NewNexoralDB.createDB("NewDB");
   // Start the DNS server and listen for incoming queries
   new DNS().start().listen().listenError();
 }
