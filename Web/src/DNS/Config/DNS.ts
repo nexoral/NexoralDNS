b592dc749c1b8c7f0a39173eddf69094f4d47b84
Ankan Saha <ankan.saha@hoichoi.tv>
2025-09-16 18:30:42 +0530

    fix(DNS): correct variable naming for DNS database and record collection

--- Web/src/DNS/Config/DNS.ts
+++ Web/src/DNS/Config/DNS.ts
@@ -4,12 +4,15 @@ import DNS from "../services/DNS.Service";
 import NewNexoralDB from "../../Database/Axiodb.config";
 // Types
 import Database from "axiodb/lib/Services/Database/database.operation";
+import Collection from "axiodb/lib/Services/Collection/collection.operation";
 
 // DB Configurations
-let DNS_Record_DB: Database;
+let DNS_DB: Database;
+let DNS_Record_Collection: Collection;
 
 export default async function startDNSServer() {
-  DNS_Record_DB = await NewNexoralDB.createDB("DNS");
+  DNS_DB = await NewNexoralDB.createDB("DNS");
+  DNS_Record_Collection = await DNS_DB.createCollection("Records");
   // Start the DNS server and listen for incoming queries
   new DNS().start().listen().listenError();
 }
