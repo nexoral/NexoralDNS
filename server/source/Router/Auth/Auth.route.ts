498bf8b30b4b88cb36fbf74fb06e77ec7c1b207c
Ankan Saha <ankansahadaluabari@gmail.com>
2025-09-30 12:49:34 +0530

    Bump version to 1.1.7-stable across all package.json files and VERSION file; implement login functionality in AuthController and LoginService.

--- server/source/Router/Auth/Auth.route.ts
+++ server/source/Router/Auth/Auth.route.ts
@@ -3,19 +3,13 @@
 import { FastifyInstance, FastifyPluginOptions, FastifyReply, FastifyRequest } from "fastify";
 import buildResponse from "../../helper/responseBuilder.helper";
 import { StatusCodes } from "outers";
+import AuthController from "../../Controller/Auth/auth.controller";
 
 export interface AuthOptions extends FastifyPluginOptions { }
 
 export default async function authRouter(fastify: FastifyInstance, _options: AuthOptions): Promise<void> {
 
   // Login Route
-  fastify.post("/login", { preHandler: [] }, async (request: FastifyRequest, reply: FastifyReply) => {
-    const FastifyResponse = new buildResponse(reply, StatusCodes.OK, "Login route - to be implemented");
-    return FastifyResponse.send(
-      {
-        message: "Login functionality is not yet implemented.",
-      },
-    );
-  });
+  fastify.post("/login", { preHandler: [] }, AuthController.login);
 
 }
