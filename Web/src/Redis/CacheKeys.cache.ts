bdf9f8177a4959174aac3727c983f86e9853f207
Ankan Saha <ankansahadaluabari@gmail.com>
2025-11-18 23:55:20 +0530

    Added New Key for caching

--- Web/src/Redis/CacheKeys.cache.ts
+++ Web/src/Redis/CacheKeys.cache.ts
@@ -1,6 +1,7 @@
 enum CacheKeys {
   Service_Status= "dns-server-status",
-  Domain_DNS_Record = "Domain_DNS_Record"
+  Domain_DNS_Record = "Domain_DNS_Record",
+  Block_Domains = "Blocked_Domain"
 }
 
 export default CacheKeys;
