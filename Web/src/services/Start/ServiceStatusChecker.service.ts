fe7aff8fcbd0032211c1023a55a564426ff30c25
Ankan Saha <ankan.saha@hoichoi.tv>
2025-12-04 23:48:43 +0530

    Added Default TTL handling in Rules.service.ts and updated ServiceStatusChecker.service.ts to use 'any' type for serviceConfig.

--- Web/src/services/Start/ServiceStatusChecker.service.ts
+++ Web/src/services/Start/ServiceStatusChecker.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Console } from "outers";
 import { DB_DEFAULT_CONFIGS } from "../../Config/key";
 import { getCollectionClient } from "../../Database/mongodb.db";
@@ -7,6 +8,12 @@ import dgram from "dgram";
 // Cache Settings
 import RedisCache from "../../Redis/Redis.cache";
 import CacheKeys from "../../Redis/CacheKeys.cache";
+
+export type ServiceStatusResult = {
+  serviceStatus: boolean,
+  serviceConfig: any | null
+}
+
 
 export default class ServiceStatusChecker {
   private readonly IO: InputOutputHandler;
@@ -24,26 +31,35 @@ export default class ServiceStatusChecker {
    * @param queryName 
    * @returns boolean
    */
-  public async checkServiceStatus(queryName: string) {
+  public async checkServiceStatus(queryName: string): Promise<ServiceStatusResult> {
     // Check Redis Cache first
     const serviceStatusCache = await RedisCache.get(CacheKeys.Service_Status);
 
     // If cache exists, use it
     if (serviceStatusCache !== null) {
-      if (!serviceStatusCache) {
+      if (serviceStatusCache.Service_Status !== "active") {
         Console.red("Service is inactive (from cache). DNS query processing is halted.");
-        this.IO.buildSendAnswer(this.msg, this.rinfo, queryName, "0.0.0.0", 5); // Respond with NXDOMAIN
-        return false;
+        this.IO.buildSendAnswer(this.msg, this.rinfo, queryName, "0.0.0.0", 10); // Respond with NXDOMAIN
+        return {
+          serviceStatus: false,
+          serviceConfig: serviceStatusCache
+        };
       }
       else {
-        return true;
+        return {
+          serviceStatus: true,
+          serviceConfig: serviceStatusCache
+        };
       }
     }
 
     const serviceCollection = getCollectionClient(DB_DEFAULT_CONFIGS.Collections.SERVICE)
     if (!serviceCollection) {
       Console.red("Service collection not found in the database.");
-      return false;
+      return {
+        serviceStatus: false,
+        serviceConfig: null
+      };
     }
 
     const serviceConfig = await serviceCollection.findOne({ SERVICE_NAME: DB_DEFAULT_CONFIGS.DefaultValues.ServiceConfigs.SERVICE_NAME });
@@ -51,18 +67,26 @@ export default class ServiceStatusChecker {
 
     if (!serviceConfig) {
       Console.red("Service configuration not found in the database.");
-      return false;
+      return {
+        serviceStatus: false,
+        serviceConfig: null
+      };
     }
 
+    await RedisCache.set(CacheKeys.Service_Status, serviceConfig);
     if (serviceConfig.Service_Status !== "active") {
       Console.red("Service is inactive. DNS query processing is halted.");
       this.IO.buildSendAnswer(this.msg, this.rinfo, queryName, "0.0.0.0", serviceConfig.DefaultTTL ? serviceConfig.DefaultTTL : 10); // Respond with NXDOMAIN
-      await RedisCache.set(CacheKeys.Service_Status, false);
-      return false;
+      return {
+        serviceStatus: false,
+        serviceConfig: serviceConfig
+      };
     }
     else {
-      await RedisCache.set(CacheKeys.Service_Status, true);
-      return true;
+      return {
+        serviceStatus: true,
+        serviceConfig: serviceConfig
+      };
     }
   }
 }
